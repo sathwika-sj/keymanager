ee84c9503220a411abae2c168272fbdbeede782b
Nagarjuna K <64436505+Nagarjunakuchi88@users.noreply.github.com>
2025-02-13 01:34:07 +0530

    Update configure_start.sh
    Signed-off-by: Nagarjuna K <64436505+Nagarjunakuchi88@users.noreply.github.com>

--- kernel/kernel-keymanager-service/configure_start.sh
+++ kernel/kernel-keymanager-service/configure_start.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
-#installs the pkcs11 libraries.
+# Installs the pkcs11 libraries.
 set -e
 
-DEFAULT_ZIP_PATH=ncipher_client.zip?rlkey=5ocm0tahzpz2hwilgkmw0j64q&st=n7bzkz47&dl=0
+DEFAULT_ZIP_PATH="ncipher_client.zip?rlkey=5ocm0tahzpz2hwilgkmw0j64q&st=n7bzkz47&dl=0"
 [ -z "$hsm_zip_file_path" ] && zip_path="$DEFAULT_ZIP_PATH" || zip_path="$hsm_zip_file_path"
 
 echo "Download the client from $artifactory_url_env"
@@ -12,21 +11,17 @@ echo "Zip File Path: $zip_path"
 wget -q --show-progress "https://www.dropbox.com/scl/fi/hiykpilg0oxs235eoj4of/$zip_path"
 echo "Downloaded https://www.dropbox.com/scl/fi/hiykpilg0oxs235eoj4of/$zip_path"
 
-export FILE_NAMES=${zip_path##*/}
+# Extract the file name from the URL
+FILE_NAME=${zip_path%%\?*}
 
-echo "FILE_NAME: $FILE_NAMES"
-
-FILE_NAME=${zip_path##*/}
-
-
-
+echo "FILE_NAME: $FILE_NAME"
 
 DIR_NAME=$hsm_local_dir_name
 
-has_parent=$(zipinfo -1 "$FILE_NAME" | awk '{split($NF,a,"?");print a[1]}' | sort -u | wc -l)
+has_parent=$(zipinfo -1 "$FILE_NAME" | awk -F/ '{print $1}' | sort -u | wc -l)
 if test "$has_parent" -eq 1; then
   echo "Zip has a parent directory inside"
-  dirname=$(zipinfo -1 "$FILE_NAME" | awk '{split($NF,a,"?");print a[1]}' | sort -u | head -n 1)
+  dirname=$(zipinfo -1 "$FILE_NAME" | awk -F/ '{print $1}' | sort -u | head -n 1)
   echo "Unzip directory"
   unzip $FILE_NAME
   echo "Renaming directory"
