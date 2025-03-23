f2e35cf799ca3b7628878078dbd68c85ee1ce961
Nagarjuna K <64436505+Nagarjunakuchi88@users.noreply.github.com>
2025-03-24 00:00:55 +0530

    Update configure_start.sh
    Signed-off-by: Nagarjuna K <64436505+Nagarjunakuchi88@users.noreply.github.com>

--- kernel/keys-generator/configure_start.sh
+++ kernel/keys-generator/configure_start.sh
@@ -3,7 +3,7 @@
 #installs the pkcs11 libraries.
 set -e
 
-DEFAULT_ZIP_PATH=artifactory/libs-release-local/hsm/client.zip
+DEFAULT_ZIP_PATH=artifactory/libs-release-local/hsm/ida_client.zip
 [ -z "$hsm_zip_file_path" ] && zip_path="$DEFAULT_ZIP_PATH" || zip_path="$hsm_zip_file_path"
 
 echo "Download the client from $artifactory_url_env"
