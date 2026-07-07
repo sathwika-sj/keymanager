930c574790f96dc20bcc2a5a443313f7541bf01b
Jagadeesh-T440 <jagadeesh.thiyagarajan@technoforte.co.in>
2026-07-07 17:09:46 +0530

    Update PartnerCertManagerErrorConstants.java
    Signed-off-by: Jagadeesh-T440 <jagadeesh.thiyagarajan@technoforte.co.in>

--- kernel/kernel-keymanager-service/src/main/java/io/mosip/kernel/partnercertservice/constant/PartnerCertManagerErrorConstants.java
+++ kernel/kernel-keymanager-service/src/main/java/io/mosip/kernel/partnercertservice/constant/PartnerCertManagerErrorConstants.java
@@ -17,9 +17,9 @@ public enum PartnerCertManagerErrorConstants {
 
 	CERTIFICATE_DATES_NOT_VALID("KER-PCM-004", "Certificate Dates are not valid."),
 
-	ROOT_CA_NOT_FOUND("KER-PCM-005", "Root CA Certificate not found."),
+	ROOT_CA_NOT_FOUND("KER-PCM-005", "Upload the correct partner certificate. Corresponding Root CA Certificate not found."),
 
-	ROOT_INTER_CA_NOT_FOUND("KER-PCM-006", "Root CA/Intermediate CA Certificates not found."),
+	ROOT_INTER_CA_NOT_FOUND("KER-PCM-006", "Upload the correct partner certificate. Corresponding Root CA/Intermediate CA certificate not found."),
 
 	INVALID_CERT_VERSION("KER-PCM-007", "Certificate version not supported."),
 
